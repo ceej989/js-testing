b63b6e08b2914500a8d0d9f35ff475d3d5b8609d
Chris John <cjohn989@gmail.com>
2012-07-12 23:48:04 -0400

    Kata 2 Recursion complete!

--- Kata/src/chop2.js
+++ Kata/src/chop2.js
@@ -1,22 +1,27 @@
 function recChop(start, end, target, intArry) {
 	var mid = Math.floor((start+end)/2);
-	if(start==end) {
+	if (target == null)
+		return -1;
+	else if(start==end) {
 		return intArry[start] == target ? start : -1;
 	}
 	else if(target > intArry[mid]) {
 		console.log("In 2nd Half");
-		start = mid;
+		start = mid+1;
 		return recChop(start, end, target, intArry);
 	}
 
 	else if(target < intArry[mid]){
 		console.log("In 1st Half");
-		end = mid;
+		end = mid-1;
 		return recChop(start,end,target,intArry);
 	}
 
 	else if(target == intArry[mid]){
 		console.log("Found "+target+"!");
+		if(target == intArry[mid-1]){
+			return recChop(start, (mid-1), target, intArry);
+		}
 		return mid;
 	}
 }
